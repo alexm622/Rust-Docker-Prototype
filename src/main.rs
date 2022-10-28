9409a9d5438648d92a6a2bcd1df5f86ab3b5721e
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2022-10-28 11:24:15 -0400

    basic tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate rs_docker;
+
+use rs_docker::Docker;
+
 fn main() {
-    println!("Hello, world!");
+    let mut docker = match Docker::connect("unix:///var/run/docker.sock") {
+        Ok(docker) => docker,
+        Err(e) => {
+            panic!("{}", e);
+        }
+    };
+    let containers = match docker.get_containers(false) {
+        Ok(containers) => containers,
+        Err(e) => {
+            panic!("{}", e);
+        }
+    };
+
+    println!("reading containers");
+
+    for container in containers {
+        println!("{}", container.Id);
+    }
 }
